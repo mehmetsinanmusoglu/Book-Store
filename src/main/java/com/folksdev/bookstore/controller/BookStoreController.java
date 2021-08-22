2914507bec5a9f638f6da849e0e4cd735f2f4714
Mehmet sinan musoglu <mehmetsinanmusoglu@gmail.com>
2021-08-22 13:35:32 +0300

    First commit

---
+++ src/main/java/com/folksdev/bookstore/controller/BookStoreController.java
@@ -0,0 +1,29 @@
+package com.folksdev.bookstore.controller;
+
+import com.folksdev.bookstore.dto.BookOrderRequest;
+import com.folksdev.bookstore.model.Order;
+import com.folksdev.bookstore.service.OrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/bookstore")
+public class BookStoreController {
+    private final OrderService orderService;
+
+    public BookStoreController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public ResponseEntity<String> helloTwitch(){
+        return ResponseEntity.ok("Hello Twitch");
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Order> putAndOrder(@RequestBody BookOrderRequest bookOrderRequest){
+        Order order = orderService.putAndOrder(bookOrderRequest.getBookIdList(), bookOrderRequest.getUserName());
+        return ResponseEntity.ok(order);
+    }
+}
